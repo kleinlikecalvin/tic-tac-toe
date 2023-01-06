c9e2e5db8b054f1a1ea9bdcb130e81e2830912ec
streetlightkids <ltj_hq@hotmail.com>
2023-01-05 16:58:14 -0800

    Creates basic tic tac toe layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PossibleWins } from "./constants";
+import { matches } from "./utils";
+import { Player } from "./types";
+// import Square from "./Sqaure";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [turn, setTurn] = React.useState<Player>("X");
+  const [spaces, setSpaces] = React.useState<Player[]>(Array(9).fill(null));
+  const updatedSpaces = [...spaces];
+  let message;
+  const isWinner = React.useMemo(() => {
+    for (let i = 0; i < PossibleWins.length; i++) {
+      const [a, b, c] = PossibleWins[i];
+      const thereIsAMatch = matches(spaces[a], spaces[b], spaces[c]);
+      if (thereIsAMatch) {
+        return thereIsAMatch;
+      }
+    }
+  }, [spaces]);
+
+  function handleTurn(index: number) {
+    updatedSpaces[index] = turn;
+    setSpaces(updatedSpaces);
+
+    if (turn === "O") {
+      setTurn("X");
+    } else {
+      setTurn("O");
+    }
+  }
+
+  if (isWinner) {
+    message = `The Champion is ${isWinner}`;
+  } else if (!updatedSpaces.includes(null) && isWinner === undefined) {
+    message = "This game is for the cats";
+  } else {
+    message = `Current Player: ${turn}`;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="board">
+        {spaces.map((value, index) => (
+          <button
+            className="square"
+            disabled={!!value || isWinner === "X" || isWinner === "O"}
+            key={index}
+            onClick={() => handleTurn(index)}
+          >
+            {value || null}
+          </button>
+        ))}
+      </div>
+      <div className="gamePlay">
+        <div className="turns">{message}</div>
+        <button
+          className="reset"
+          onClick={() => {
+            setTurn("X");
+            setSpaces(Array(9).fill(null));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
