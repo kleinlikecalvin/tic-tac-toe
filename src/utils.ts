c4ee12b6e48fb3d0d566d7ff7fe8e4ddec5d5e04
streetlightkids <ltj_hq@hotmail.com>
2023-08-02 16:26:30 -0700

    Updates

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Player } from "./types";
-export function matches(a: Player, b: Player, c: Player) {
-  if (a === null) return false;
-  if (b !== a) return false;
-  if (c !== b) return false;
-  return a;
-}
